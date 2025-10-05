2f19c3898528b8707ae86abc37c8300080c8654c
larissa-felixl <larissa.lima145@aluno.ce.gov.br>
2025-10-05 13:31:23 -0300

    Alterações no productController.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     public function index(){
-        $products = Product::all();       return view('products.index', ['products' => $products]);
+        $products = Product::all();       
+        return view('products.index', ['products' => $products]);
     }
 
     public function create(){
